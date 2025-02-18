e520d3bcaa0e304622fa7735ac355f7e0624f294
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2025-02-18 13:33:49 +0100

    Cleanup imports

--- src/test/kotlin/FirstTest.kt
+++ src/test/kotlin/FirstTest.kt
@@ -1,18 +1,4 @@
-import com.intellij.driver.sdk.ui.components.common.ideFrame
-import com.intellij.driver.sdk.ui.components.elements.jBlist
-import com.intellij.driver.sdk.ui.components.elements.popup
-import com.intellij.driver.sdk.ui.present
-import com.intellij.driver.sdk.ui.shouldBe
-import com.intellij.driver.sdk.ui.xQuery
-import com.intellij.driver.sdk.waitForIndicators
-import com.intellij.ide.starter.driver.engine.runIdeWithDriver
-import com.intellij.ide.starter.ide.IdeProductProvider
-import com.intellij.ide.starter.models.TestCase
-import com.intellij.ide.starter.project.GitHubProject
-import com.intellij.ide.starter.runner.Starter
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import kotlin.time.Duration.Companion.minutes
 
 class FirstTest {
     @Test
